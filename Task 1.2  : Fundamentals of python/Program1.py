d394acf4d1e71c4a3b5f6bbeccd9a3a6f4818b08
Aishwarya Patle <86393603+aishwaryapatle@users.noreply.github.com>
2022-07-12 19:46:30 +0530

    Update Program1.py

--- Task 1.2  : Fundamentals of python/Program1.py
+++ Task 1.2  : Fundamentals of python/Program1.py
@@ -1,13 +1,22 @@
+# Question: You have to given a String and you have to swap that String In Other words Convert all the lowercase letter into Uppercase letter vice versa
+
+# Sample Input :
+# Hello Code Family
+
+# Constraint :
+# 0<=len(s)<=1000
+
+# Sample Output ;
+# hELLO cODE fAMILY
+
 oldStr = input('Enter an input string : ')
 newStr = "";  
 
-   
 for i in range(0, len(oldStr)):  
     if oldStr[i].islower():  
         newStr += oldStr[i].upper();  
     elif oldStr[i].isupper():  
-        newStr += oldStr[i].lower();  
-      
+        newStr += oldStr[i].lower();      
     else:  
         newStr += oldStr[i];          
 print("String after case conversion : " +  newStr);  
